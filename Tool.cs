2fb2644f526e80d7ea823fb7277589860b5e55d5
619903036 <619903036@qq.com>
2025-06-02 13:42:57 +0800

    增加文件操作方法和物品获取功能
    在 `Tool.cs` 文件中，添加了新的方法以支持文件的读取、写入和追加写入操作，并在失败时记录错误信息。引入了 `Items` 枚举以表示背包和快捷栏，并新增 `GetItemStacks` 方法，允许获取玩家的物品列表，支持选择获取背包或快捷栏的物品。

--- Tool.cs
+++ Tool.cs
@@ -1,11 +1,13 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using static AIDirectorPlayerInventory;
 
 namespace SevenDayKillModDevelopmentTemplate
 {
@@ -14,9 +16,8 @@ namespace SevenDayKillModDevelopmentTemplate
     /// </summary>
     public static class Tool
     {
-
         /// <summary>
-        /// 创建一个文件并写入文本,文件存在时则不执行
+        /// 尝试创建一个文件并写入文本,文件存在时则不执行
         /// </summary>
         /// <param name="path"></param>
         /// <returns></returns>
@@ -25,6 +26,71 @@ namespace SevenDayKillModDevelopmentTemplate
             if (!File.Exists(path))
                 File.WriteAllText(path, str);
             return new FileInfo(path);
+        }
+        /// <summary>
+        /// 尝试读取文件,如果文件不存在则返回空字符串
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static string TryReadFile(this string path)
+        {
+            if (File.Exists(path))
+            {
+                try
+                {
+                    return File.ReadAllText(path);
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError($"读取文件失败: {e.Message}");
+                    return string.Empty;
+                }
+            }
+            else
+            {
+                Debug.LogWarning($"文件不存在: {path}");
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// 尝试写入文件,写入成功会返回true,失败会返回false
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool TryWriteFile(this string path, string str)
+        {
+            try
+            {
+                File.WriteAllText(path, str);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"写入文件失败: {e.Message}");
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 尝试追加写入文件,追加成功会返回true,失败会返回false
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool TryAppendFile(this string path, string str)
+        {
+            try
+            {
+                File.AppendAllText(path, str);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"追加写入文件失败: {e.Message}");
+                return false;
+            }
         }
 
         /// <summary>
@@ -89,6 +155,38 @@ namespace SevenDayKillModDevelopmentTemplate
         /// <returns></returns>
         public static List<Entity> GetEntitieALL() =>
             GameManager.Instance.World.Entities.list;
+
+        public enum Items
+        {
+            /// <summary>
+            /// 背包
+            /// </summary>
+            Bag,
+            /// <summary>
+            /// 快捷栏
+            /// </summary>
+            QuickBar
+        }
+
+        /// <summary>
+        /// 获取玩家背包或快捷栏所有物品
+        /// </summary>
+        /// <param name="items">获取类型,默认获取背包物品</param>
+        /// <returns></returns>
+        public static List<ItemStack> GetItemStacks(Items items = Items.Bag)
+        {
+            var localPlayer = GetlocalPlayer();
+            switch (items)
+            {
+                case Items.Bag:
+                    return localPlayer.bag.items.ToList();
+                case Items.QuickBar:
+                    return localPlayer.inventory.GetSlots().ToList();
+                default:
+                    return null;
+            }
+        }
+
 
         /// <summary>
         /// 获取本地玩家
